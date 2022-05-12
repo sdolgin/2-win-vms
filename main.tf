80cbe0c7ae5db99c0cd72a17075fa7100fea8b93
Saul Dolgin <576449+sdolgin@users.noreply.github.com>
2022-05-11 20:48:07 -0400

    add policy assignment

--- main.tf
+++ main.tf
@@ -16,6 +16,10 @@ terraform {
 
 data "azurerm_client_config" "current" {}
 
+data "azurerm_policy_set_definition" "enable_azure_monitor" {
+  display_name = "Enable Azure Monitor for VMs"
+}
+
  resource "azurerm_resource_group" "test" {
    name     = "rg-winvm-demo"
    location = "East US 2"
@@ -160,4 +164,18 @@ resource "azurerm_log_analytics_workspace" "test" {
   name                = "workspace-winvm-demo"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
+}
+
+resource "azurerm_resource_group_policy_assignment" "test" {
+  name                 = "${data.azurerm_policy_set_definition.enable_azure_monitor.display_name}"
+  resource_group_id    = azurerm_resource_group.test.id
+  policy_definition_id = "${data.azurerm_policy_set_definition.enable_azure_monitor.id}"
+
+  parameters = <<PARAMS
+    {
+      "logAnalytics_1": {
+        "value": "${azurerm_log_analytics_workspace.test.id}"
+      }
+    }
+PARAMS
 }
