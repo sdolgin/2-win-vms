d22768d43b75e132f68f9c619f4e283b2e291936
Saul Dolgin <576449+sdolgin@users.noreply.github.com>
2023-07-14 16:27:58 -0400

    updates to use with monstar pack testing

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 terraform {
+  required_version = ">=0.12"
 
-   required_version = ">=0.12"
-
-   required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "=3.5.0"
-     }
-   }
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=3.5.0"
+    }
+  }
  }
 
  provider "azurerm" {
@@ -18,8 +17,8 @@ terraform {
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "test" {
-  name     = "rg-winvm-demo-jermaine"
-  location = "East US 2"
+  name     = "rg-winvm-demo"
+  location = "North Central US"
 }
 
 resource "azurerm_virtual_network" "test" {
@@ -43,106 +42,62 @@ resource "azurerm_subnet" "bastion" {
   address_prefixes     = ["10.0.3.0/24"]
 }
 
-resource "azurerm_public_ip" "bastion" {
-  name                = "pip-bastion"
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
-}
-
-resource "azurerm_bastion_host" "test" {
-  name                = "bastion-winvm-demo"
+resource "azurerm_network_interface" "test" {
+  count               = 2
+  name                = "nic-winvm-demo-${count.index}"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
 
   ip_configuration {
-    name                 = "configuration"
-    subnet_id            = azurerm_subnet.bastion.id
-    public_ip_address_id = azurerm_public_ip.bastion.id
+    name                          = "primaryIp"
+    subnet_id                     = azurerm_subnet.test.id
+    private_ip_address_allocation = "Dynamic"
   }
-}
-
-resource "azurerm_network_interface" "test" {
-   count               = 2
-   name                = "nic-winvm-demo-${count.index}"
-   location            = azurerm_resource_group.test.location
-   resource_group_name = azurerm_resource_group.test.name
-
-   ip_configuration {
-     name                          = "testConfiguration"
-     subnet_id                     = azurerm_subnet.test.id
-     private_ip_address_allocation = "Dynamic"
-   }
- }
-
-resource "azurerm_managed_disk" "test" {
-   count                = 2
-   name                 = "datadisk_existing_${count.index}"
-   location             = azurerm_resource_group.test.location
-   resource_group_name  = azurerm_resource_group.test.name
-   storage_account_type = "Premium_LRS"
-   create_option        = "Empty"
-   disk_size_gb         = "1023"
- }
-
-resource "azurerm_availability_set" "avset" {
-   name                         = "avset"
-   location                     = azurerm_resource_group.test.location
-   resource_group_name          = azurerm_resource_group.test.name
-   platform_fault_domain_count  = 2
-   platform_update_domain_count = 2
-   managed                      = true
  }
 
 resource "azurerm_virtual_machine" "test" {
-   count                 = 2
-   name                  = "vm-win-demo-${count.index}"
-   location              = azurerm_resource_group.test.location
-   availability_set_id   = azurerm_availability_set.avset.id
-   resource_group_name   = azurerm_resource_group.test.name
-   network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
-   vm_size               = "Standard_DS2_v2"
+  count                 = 2
+  name                  = "vm-win-demo-${count.index}"
+  location              = azurerm_resource_group.test.location
+  resource_group_name   = azurerm_resource_group.test.name
+  network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
+  vm_size               = "Standard_B2s"
 
-   # Uncomment this line to delete the OS disk automatically when deleting the VM
-   # delete_os_disk_on_termination = true
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
 
-   # Uncomment this line to delete the data disks automatically when deleting the VM
-   # delete_data_disks_on_termination = true
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
 
-   storage_image_reference {
-     publisher = "MicrosoftWindowsServer"
-     offer     = "WindowsServer"
-     sku       = "2019-Datacenter"
-     version   = "latest"
-   }
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
 
-   storage_os_disk {
-     name              = "myosdisk${count.index}"
-     caching           = "ReadWrite"
-     create_option     = "FromImage"
-     managed_disk_type = "Premium_LRS"
-   }
+  storage_os_disk {
+    name              = "myosdisk${count.index}"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
 
-   storage_data_disk {
-     name            = element(azurerm_managed_disk.test.*.name, count.index)
-     managed_disk_id = element(azurerm_managed_disk.test.*.id, count.index)
-     create_option   = "Attach"
-     lun             = 1
-     disk_size_gb    = element(azurerm_managed_disk.test.*.disk_size_gb, count.index)
-   }
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "testadmin"
+    admin_password = "Password1234!"
+  }
 
-   os_profile {
-     computer_name  = "hostname"
-     admin_username = "testadmin"
-     admin_password = "Password1234!"
-   }
+  os_profile_windows_config {
+    provision_vm_agent = true
+  }
 
-   os_profile_windows_config {
-     provision_vm_agent = true
-   }
+  identity {
+    type = "SystemAssigned"
+  }
 
-   tags = {
-     environment = "staging"
-   }
+  tags = {
+    environment = "staging"
+  }
 }
